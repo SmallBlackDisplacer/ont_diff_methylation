11e2d62b72f12394f08f7999de2d5ca50a56b3bd
Wojard <tanarri.nabassu@gmail.com>
2020-04-03 00:20:01 +0300

    Fix binary flag filtration: from 256 with duplicates to 3844 without duplicates. Remove pandas usage. Adding a haplotype to a file is three times faster

--- add_ht.py
+++ add_ht.py
@@ -1,6 +1,5 @@
 import pysam
 import argparse
-import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-hb", "--haplotype_bam", type=str)
@@ -12,25 +11,13 @@ haplo_dic = {}
 samfile = pysam.AlignmentFile(args.haplotype_bam, "rb")
 
 for read in samfile:
-    if read.is_secondary:
-        continue
-    hpt = str(read.get_tag('HP')) if read.has_tag('HP') else None
-    rname = str(read.qname)
-    if rname in haplo_dic:
-        if not hpt:
-            continue
-        if not haplo_dic[rname]:
-            haplo_dic[rname] = hpt
-        elif haplo_dic[rname] != hpt:
-            haplo_dic[rname] = None
-    else:
+    if read.flag & 3844 == 0:
+        hpt = str(read.get_tag('HP')) if read.has_tag('HP') else None
+        rname = str(read.qname)
         haplo_dic[rname] = hpt
 
-met_calls = pd.read_csv(args.methylation_tsv, sep="\t")
-
-Haplotype = []
-for read in met_calls.read_name:
-    Haplotype.append(haplo_dic[read] if read in haplo_dic else None)
-met_calls['Haplotype'] = Haplotype
-
-met_calls.to_csv(args.output_file_name, sep='\t')
+with open (args.methylation_tsv, 'r') as mc, open (args.output_file_name, 'w') as mh:
+    mh.write(mc.readline().strip() + '\tHaplotype\n')
+    for line in mc:
+        read = line.split('\t')[4]
+        mh.write(line.strip() + '\t' + str(haplo_dic[read] if read in haplo_dic else None) + '\n')

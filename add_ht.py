4986c4cea82d8ce6868c82ca30cc7106c3321dda
Timofey Prodanov <timofey.prodanov@gmail.com>
2020-04-03 18:26:21 +0200

    Refactor code

--- add_ht.py
+++ add_ht.py
@@ -1,23 +1,33 @@
+#!/usr/bin/env python3
+
 import pysam
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-hb", "--haplotype_bam", type=str)
-parser.add_argument("-mt", "--methylation_tsv", type=str)
-parser.add_argument("-o", "--output_file_name", type=str)
-args = parser.parse_args()
 
-haplo_dic = {}
-samfile = pysam.AlignmentFile(args.haplotype_bam, "rb")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-h", "--haplotype_bam", required=True,
+            help='....')
+    parser.add_argument("-m", "--methylation_tsv")
+    parser.add_argument("-o", "--output_file_name")
+    args = parser.parse_args()
 
-for read in samfile:
-    if read.flag & 3844 == 0:
-        hpt = str(read.get_tag('HP')) if read.has_tag('HP') else None
-        rname = str(read.qname)
-        haplo_dic[rname] = hpt
+    haplo_dic = {}
+    samfile = pysam.AlignmentFile(args.haplotype_bam)
 
-with open (args.methylation_tsv, 'r') as mc, open (args.output_file_name, 'w') as mh:
-    mh.write(mc.readline().strip() + '\tHaplotype\n')
-    for line in mc:
-        read = line.split('\t')[4]
-        mh.write(line.strip() + '\t' + str(haplo_dic[read] if read in haplo_dic else None) + '\n')
+    for read in samfile:
+        if read.flag & 3844 == 0:
+            hpt = str(read.get_tag('HP')) if read.has_tag('HP') else None
+            rname = str(read.qname)
+            haplo_dic[rname] = hpt
+
+    with open(args.methylation_tsv, 'r') as mc, open(args.output_file_name, 'w') as mh:
+        mh.write(mc.readline().strip() + '\tHaplotype\n')
+        for line in mc:
+            read = line.split('\t')[4]
+            mh.write(line.strip() + '\t' + str(haplo_dic[read] if read in haplo_dic else None) + '\n')
+
+
+if __name__ == '__main__':
+    main()
+
